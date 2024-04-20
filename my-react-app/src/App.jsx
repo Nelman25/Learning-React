9dd9084271c04f6f184073173b8d0b8a2a51a978
Nelman25 <jonelvillaver735@gmail.com>
2024-04-20 18:42:58 +0800

    props, propTypes, and defaultProps

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,12 @@
-import Card from "./Card.jsx";
+import Student from "./Student.jsx";
 
 function App() {
   return (
-    <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </>
+  <>
+    <Student name="Dharilyn Taladro" age={19} isStudent={true}/>
+    <Student name="Jonel Villaver"  age={19} isStudent={true}/>
+    <Student name="BongBong Marcos" age={99} isStudent={true}/>
+  </>
   );
 }
 
