16ba9af9621c52dea4fd44a3079bf7c97277ac78
Nelman25 <jonelvillaver735@gmail.com>
2024-04-26 13:47:07 +0800

    added proptypes and default props

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 function List(props) {
   
     const itemList = props.items;
@@ -23,7 +24,16 @@ function List(props) {
     <ol className="list-items">{listItems}</ol>
     </>
   )
-  
+}
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number,
+                                               name: PropTypes.string,
+                                               calories: PropTypes.number}))
+}
+List.defaultProps = { 
+    category: "Category",
+    items: [],
 }
 
 export default List;
