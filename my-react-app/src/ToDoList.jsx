c88bcfc1efc9e36a8f8e59164a5a77453f561b87
Nelman25 <jonelvillaver735@gmail.com>
2024-05-08 12:30:23 +0800

    to-do-list react

--- my-react-app/src/ToDoList.jsx
+++ my-react-app/src/ToDoList.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
 function ToDoList() {
-    const [tasks, setTasks] = useState(["Eat breakfast","take a shower"]);
+    const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
         setNewTask(event.target.value);
     }
     function addTask() {
-
+       if(newTask.trim() !== "") {
+        setTasks(tasks => [...tasks, newTask]);
+        setNewTask("");
+       }
     }
     function deleteTask(index) {
+        const updatedTask = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTask);
 
     }
     function moveTaskUp(index) {
-
+        if(index > 0) {
+            const updatedTask = [...tasks];
+            [updatedTask[index], updatedTask[index - 1]] = 
+            [updatedTask[index - 1], updatedTask[index]];
+            setTasks(updatedTask);
+        }
     }
     function moveTaskDown(index) {
-
+        if(index < tasks.length - 1) {
+            const updatedTask = [...tasks];
+            [updatedTask[index], updatedTask[index + 1]] = 
+            [updatedTask[index + 1], updatedTask[index]];
+            setTasks(updatedTask);
+        }
     }
 
     return(
@@ -29,7 +44,7 @@ function ToDoList() {
             <ol>
                 {tasks.map((task, index) =>
                     <li key={index}>
-                        <span>{task}</span>
+                        <span className='text'>{task}</span>
                         <button className='delete-button' onClick={() => deleteTask(index)}>Delete</button>
                         <button className='move-button' onClick={() => moveTaskUp(index)}>⬆️</button>
                         <button className='move-button' onClick={() => moveTaskDown(index)}>⬇️</button>
