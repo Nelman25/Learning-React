493eb42794aa1f3aef47833edaf8c280159d581e
Nelman25 <jonelvillaver735@gmail.com>
2024-05-18 17:09:17 +0800

    learning useRef react hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,5 @@
-//  useContext() = React hook that allows you to share values
-//                 between multiple levels of components
-//                 without passing props through each level
-
-//  PROVIDER COMPONENT
-//  1. import {createContext} from 'react';
-//  2. export const MyContext = createContext();
-//  3. <MyContext.Provider value={value}>
-//          <Child/>
-//     </MyContext.Provider value={value}>
-
-
-//  CONSUMER COMPONENTS 
-//  1. import React, { userContext } from 'react';
-//  2. import { MyContext } from './ComponentA.jsx'
-//  3. const value = useContext(MyContext);
-
-import ComponentA from "./ComponentA";
-import React from 'react';
-
+import MyComponent from "./MyComponent"
 function App(){
-  return(
-    <ComponentA/>
-  );
-}export default App
+  return(<MyComponent/>)
+}
+export default App
---
+++ my-react-app/src/MyComponent.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function MyComponent(){
+
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    useEffect(() => {
+        console.log('Component Rendered');
+    })
+
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "lightBlue"
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = ""
+
+    }
+
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = "lightBlue"
+        inputRef3.current.style.backgroundColor = ""
+
+    }
+
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = "lightBlue"    }
+    return(
+        <div>
+            <button onClick={handleClick1}>Click Me!</button>
+            <input ref={inputRef1} /><br />
+
+            <button onClick={handleClick2}>Click Me!</button>
+            <input ref={inputRef2} /><br />
+
+            <button onClick={handleClick3}>Click Me!</button>
+            <input ref={inputRef3} />
+        </div>
+    )
+}
+export default MyComponent
